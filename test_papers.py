3c5863a629468fd1ec78f973cf105fccf819e5af
olenakit <olenakit@hotmail.com>
2014-11-06 20:37:27 -0500

    Added more test cases

--- test_papers.py
+++ test_papers.py
@@ -11,34 +11,41 @@ __status__ = "Prototype"
 import pytest
 from papers import decide
 
+
+# Basic tests
 def test_basic():
     assert decide("test_returning_citizen.json", "watchlist.json", "countries.json") == ["Accept", "Accept"]
     assert decide("test_watchlist.json", "watchlist.json", "countries.json") == ["Secondary"]
     assert decide("test_quarantine.json", "watchlist.json", "countries.json") == ["Quarantine"]
 
+
+# Test when files are not found
 def test_files():
     with pytest.raises(FileNotFoundError):
-        decide("test_returning_citizen.json","","")
-    with pytest.raises(FileNotFoundError):
+        decide("", "", "")
+        decide("", "watchlist.json", "")
+        decide("", "", "countries.json")
+        decide("", "watchlist.json", "countries.json")
+        decide("test_returning_citizen.json", "", "")
         decide("test_returning_citizen.json", "", "countries.json")
-    with pytest.raises(FileNotFoundError):
         decide("test_returning_citizen.json", "watchlist.json", "")
-
-    with pytest.raises(FileNotFoundError):
-        decide("test_watchlist.json","","")
-    with pytest.raises(FileNotFoundError):
+        decide("test_watchlist.json", "", "")
         decide("test_watchlist.json", "", "countries.json")
-    with pytest.raises(FileNotFoundError):
         decide("test_watchlist.json", "watchlist.json", "")
-
-    with pytest.raises(FileNotFoundError):
-        decide("test_quarantine.json","","")
-    with pytest.raises(FileNotFoundError):
+        decide("test_quarantine.json", "", "")
         decide("test_quarantine.json", "", "countries.json")
-    with pytest.raises(FileNotFoundError):
         decide("test_quarantine.json", "watchlist.json", "")
 
 
-# add functions for other tests
-# Add tests to check for incomplete entries
-# ex. missing passport, name being blank, missing region in location entry
+# Test when files are incomplete
+def test_incomplete():
+    # Test blank entries
+    ### Index 0: Blank passport
+    ### Index 1: Blank name
+    ### Index 2: Blank location
+    ### Index 3: Blank entry reason
+    assert decide("test_blank.json", "watchlist.json", "countries.json") == ["Reject", "Reject", "Reject", "Reject"]
+    # Test invalid entries
+    ### Index 0: Invalid passport
+    ### Index 1: Invalid birth date
+    assert decide("test_invalid.json", "watchlist.json", "countries.json") == ["Reject", "Reject"]
